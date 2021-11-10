8f686eecf34e5a6b78ffc66937d3a14be027f6be
Belen LB <blaserna@iessonferrer.net>
2021-11-10 20:28:44 +0100

    Clase 10/11/2021

---
+++ 6_FibonacciRecursiva/js/fibonacci.js
@@ -0,0 +1,26 @@
+function fibonacci(x) {
+    if (x==0) return 0;
+    else {
+        if (x==1) return 1;
+        else return fibonacci(x-1)+fibonacci(x-2);
+    }
+}
+
+function fibonacci2(x) {
+    let acumulador1=0;
+    let acumulador2=1;
+    let resultado=1;
+
+    if (x==0) return 0;
+    else {
+        if (x==1) return 1;
+        else {
+            for (let i=2;i<=x;i++) {
+                [acumulador1,acumulador2] = [acumulador2,resultado];
+                resultado = acumulador1 + acumulador2;
+            }
+            
+        return resultado;
+        }
+    }
+}
